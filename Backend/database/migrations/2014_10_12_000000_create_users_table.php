1489ed00dbc595c997e5c001ebe37a6116d5c817
Daniel <dev.danielrod@gmail.com>
2024-10-13 21:53:26 -0400

    Registra usuario modificado

--- Backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ Backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('username');
-            $table->string('name');
+            $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password');
