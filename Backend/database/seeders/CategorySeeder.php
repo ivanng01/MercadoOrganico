9af3598bf48895b6f8e410eaf0c474fb80894cbb
maxcerecedadev <cereceda1991@gmail.com>
2024-10-22 00:52:44 -0500

    feat: update docs and routes

--- Backend/database/seeders/CategorySeeder.php
+++ Backend/database/seeders/CategorySeeder.php
@@ -2,30 +2,61 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        Category::create([
-            'name' => 'Naturales',
-            'description' => 'Categoria de naturales',
-            'parent_id' => 1,
-        ]);
+        Category::updateOrCreate(
+            ['name' => 'Huevos'],
+            ['description' => 'Incluye productos a base de huevo.', 'parent_id' => null]
+        );
 
-        Category::updateOrCreate([
-            'name' => 'Sinteticos',
-            'description' => 'Categoria de sinteticos',
-            'parent_id' => 2,
-        ]);
+        Category::updateOrCreate(
+            ['name' => 'Verduras'],
+            ['description' => 'Abarca todo tipo de vegetales.', 'parent_id' => null]
+        );
 
+        Category::updateOrCreate(
+            ['name' => 'Lácteos y Sustitutos'],
+            ['description' => 'Productos lácteos y alternativas veganas.', 'parent_id' => null]
+        );
+
+        Category::updateOrCreate(
+            ['name' => 'Condimentos y Salsas'],
+            ['description' => 'Especias, salsas y aderezos.', 'parent_id' => null]
+        );
+
+        Category::updateOrCreate(
+            ['name' => 'Carnes y Sustitutos'],
+            ['description' => 'Carnes y opciones vegetarianas o veganas.', 'parent_id' => null]
+        );
+
+        Category::updateOrCreate(
+            ['name' => 'Miel y Edulcorantes'],
+            ['description' => 'Miel, azúcar y otros endulzantes.', 'parent_id' => null]
+        );
+
+        Category::updateOrCreate(
+            ['name' => 'Bebidas'],
+            ['description' => 'Diversas bebidas, como agua, jugos, etc.', 'parent_id' => null]
+        );
+
+        Category::updateOrCreate(
+            ['name' => 'Productos de Panadería'],
+            ['description' => 'Pan, bollería y productos horneados.', 'parent_id' => null]
+        );
+
+        Category::updateOrCreate(
+            ['name' => 'Frutas'],
+            ['description' => 'Todo tipo de frutas frescas.', 'parent_id' => null]
+        );
+
+        Category::updateOrCreate(
+            ['name' => 'Granos y Legumbres'],
+            ['description' => 'Cereales, legumbres y semillas.', 'parent_id' => null]
+        );
     }
 }
