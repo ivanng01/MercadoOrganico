fbe996771a0ab447569915963f541dd79e758387
maxcerecedadev <cereceda1991@gmail.com>
2024-10-24 12:00:48 -0500

    docs: optmizing images files

--- Frontend/src/pages/public/data/galleryPhotos.ts
+++ Frontend/src/pages/public/data/galleryPhotos.ts
@@ -1,32 +1,32 @@
 export const galleryPhotos = [
   {
     id: 1,
-    imageUrl: "/gallery/1.png",
+    imageUrl: "/gallery/1.webp",
     altText: "Descripción de la foto 1",
   },
   {
     id: 2,
-    imageUrl: "/gallery/2.png",
+    imageUrl: "/gallery/2.webp",
     altText: "Descripción de la foto 2",
   },
   {
     id: 3,
-    imageUrl: "/gallery/3.png",
+    imageUrl: "/gallery/3.webp",
     altText: "Descripción de la foto 3",
   },
   {
     id: 4,
-    imageUrl: "/gallery/4.png",
+    imageUrl: "/gallery/4.webp",
     altText: "Descripción de la foto 4",
   },
   {
     id: 5,
-    imageUrl: "/gallery/5.png",
+    imageUrl: "/gallery/5.webp",
     altText: "Descripción de la foto 5",
   },
   {
     id: 6,
-    imageUrl: "/gallery/6.png",
+    imageUrl: "/gallery/6.webp",
     altText: "Descripción de la foto 6",
   },
 ];
