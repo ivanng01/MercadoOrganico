70cf517ac9172b02360dc6f41bfa9fa1a2ecf28f
maxcerecedadev <cereceda1991@gmail.com>
2024-10-26 08:16:20 -0500

    refactor : update list results with pagination

--- Frontend/src/pages/public/components/product/NoProductsFound.tsx
+++ Frontend/src/pages/public/components/product/NoProductsFound.tsx
@@ -5,7 +5,7 @@ export default function NoProductsFound() {
   return (
     <div className="w-full flex flex-col items-center justify-start text-center pt-20 h-screen">
       <PackageX className="w-24 h-24 text-muted-foreground mb-6" />
-      <h2 className="text-2xl font-bold text-white mb-2">No se encontraron productos</h2>
+      <h2 className="text-2xl font-bold text-card-foreground mb-2">No se encontraron productos</h2>
       <p className="text-gray-400 mb-6">Lo sentimos, no hemos podido encontrar productos en categoría.</p>
       <div className="inline-block space-x-2">
         <Button>Explorar otras categorías</Button>
